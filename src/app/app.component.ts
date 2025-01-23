e00f3eb82fb25966d93898677528068e5f2b92b8
Josh Colvin <joshcolvin@dswinc.com>
2025-01-23 12:27:46 -0500

    feat: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,53 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'image-test';
+  canvas: HTMLCanvasElement | undefined;
+context: CanvasRenderingContext2D | undefined | null;
+player: HTMLVideoElement | undefined | null;
+
+  ngAfterViewInit() {
+    this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    this.context = this.canvas?.getContext('2d');
+    this.player = document.getElementById('player') as HTMLVideoElement;
+
+    const constraints = {
+      video: {
+        facingMode: {
+          exact: 'environment'
+        }
+      }
+    };
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+      if (this.player) {
+        this.player.srcObject = stream;
+      }
+    });
+  }
+
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const img = new Image();
+        img.onload = () => {
+          if (this.context) {
+            this.context.drawImage(img, 0, 0, this.canvas?.width as number, this.canvas?.height as number);
+          }
+        };
+        img.src = e.target?.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  capture() {
+    if (this.context && this.player && this.canvas) {
+      this.context.drawImage(this.player, 0, 0, this.canvas.width, this.canvas.height);
+    }
+
+  }
 }
